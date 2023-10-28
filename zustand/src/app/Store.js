ef226b67671fab4e3efb4fecc1c53a4641565eef
LONGMANKE <mburunjoroge0@gmail.com>
2023-10-28 14:42:27 +0300

    add, remove and toggle items from the store

--- zustand/src/app/Store.js
+++ zustand/src/app/Store.js
@@ -1,4 +1,33 @@
-import create from 'zustand';
-import {devtools, persist} from 'zustand/middleware';
+import {create, useStore } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 // creating the store
-const Store =() => ()
+const Store = (set) => ({
+    courses: [],
+    addCourse: (course) => {
+        set((state) => ({
+            courses: [course, ...state.courses],
+        }))
+    },
+    removeCourse: (courseId) => {
+        set((state) => ({
+            courses: state.courses.filter((c) => c.id !== courseId)
+        }))
+    },
+    toggleCourseStatus: (courseId) => {
+        set((state) => ({
+            courses: state.courses.map((course) => course.id === completed ? {
+                 ...course, completed: !course.completed } : course)
+        }))
+    }
+})
+
+const useStore =create(
+    devtools(
+        persist(Store, {
+            name:"courses"
+        })
+    )
+)
+
+ 
+export default useStore ;
